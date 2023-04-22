e71c2eb7a8c3a485230985d30709f4d062315fe3
JimmyLin <disillusioned2077@gmail.com>
2023-04-22 17:17:03 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+// dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post('/', (req, res) => {
+  // 抓取bodyParser資料
+  console.log('req.body', req.body)
+  res.render('index')
+})
+
+// template engine 模板引擎 handlebars
+app.engine('hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3500, () => {
+  console.log('app is running on port 3500')
+})
